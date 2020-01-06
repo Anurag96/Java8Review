6ae6bc7fb7b98b826fd8f8a73efcee0655003bb2
anuragkumar96 <s531372@nwmissouri.edu>
2020-01-05 22:49:05 -0600

    Using parallelStreams

--- src/streams/StreamsExample1.java
+++ src/streams/StreamsExample1.java
@@ -10,14 +10,23 @@ public class StreamsExample1 {
 
         List<Person> people = Arrays.asList(
                 new Person("Charles", "Dickens", 22),
-                new Person("Aharles", "Aickens", 23),
+                new Person("Aharles", "Cickens", 23),
                 new Person("Bharles", "Bickens", 24),
                 new Person("Dharles", "Cickens", 25),
                 new Person("Eharles", "Eickens", 26)
         );
 
+        // Streams : A sequence of elements supporting sequential and parallel aggregattion operations.
         people.stream()
                 .filter(p-> p.getLastName().startsWith("C"))
+                .filter(p->p.getFirstName().startsWith("A"))
                 .forEach(p -> System.out.println(p.getFirstName()));
+
+        long count = people.parallelStream()
+                .filter(p-> p.getLastName().startsWith("C"))
+                .filter(p->p.getFirstName().startsWith("A"))
+                .count();
+        System.out.println(count);
+
 }
 }
